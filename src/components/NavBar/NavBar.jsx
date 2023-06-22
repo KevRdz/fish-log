cbf2bdab284894bddea3336526ed844102eb8398
Kevin <kevin.rodriguez678@gmail.com>
2023-06-22 19:43:17 -0400

    update navbar and create addfish page

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,7 +6,9 @@ const NavBar = ({ user, handleLogout }) => {
     <nav>
       {user ?
         <ul>
-          <li>Welcome, {user.name}</li>
+          <li><NavLink to="/">Welcome, {user.name}</NavLink></li>
+          <li><NavLink to="/fish">View Fish</NavLink></li>
+          <li><NavLink to="/add">Add Catch</NavLink></li>
           <li><NavLink to="/profiles">Profiles</NavLink></li>
           <li><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></li>
           <li><NavLink to="/auth/change-password">Change Password</NavLink></li>
---
+++ src/pages/AddFish/AddFish.jsx
@@ -0,0 +1,11 @@
+
+
+const AddFish = () => {
+  return (
+    <>
+      
+    </>
+  );
+}
+
+export default AddFish;
