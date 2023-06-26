3c71ab01a7fc1fdbb55ac9661fbaa6515f51810a
Kevin <kevin.rodriguez678@gmail.com>
2023-06-26 15:22:43 -0400

    create fish list ui

--- src/pages/FishList/FishList.jsx
+++ src/pages/FishList/FishList.jsx
@@ -1,9 +1,19 @@
+import styles from '../FishList/FishList.module.css'
 
-
-const FishList = () => {
+const FishList = (props) => {
   return (
     <>
-      
+      <h1>Fish List</h1>
+      <div className={styles.container}>
+        {props.fishes.map(fish => 
+          <div key={fish._id}>
+            <p>Fish Type: {fish.type}</p>
+            <p>Fish Weight: {fish.weight}</p>
+            <p>Reel Used: {fish.reel}</p>
+            <p>Bait Used: {fish.bait}</p>
+          </div>
+        )}
+      </div>
     </>
   );
 }
